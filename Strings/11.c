1397af0bf7c6da596f69ad0b7143ec28c7c1f08f
alanacca <44409612+alanacca@users.noreply.github.com>
2019-10-31 08:41:02 -0300

    Update 11.c

--- Strings/11.c
+++ Strings/11.c
@@ -5,10 +5,10 @@
 #include <ctype.h>
 
 int main(){
-	char string[1000],newString[1000],cont[10];
+	char string[1000],newString[1000],cont[10],var3[10];
 	printf("Digite a frase:\n");
 	scanf("%s",string);
-	int count = 0;
+	int count = 0,var1,var2;
 	int j=0;
 
 	for(int i=0;i<=strlen(string);i++){
@@ -18,10 +18,23 @@ int main(){
 			}else if(string[i]!=string[i-1] || string[i] =='\0'){
 				newString[j] = string[i-1];
 				j++;
-				sprintf(cont,"%d",count);
-				strcpy(&newString[j],cont);
-				j++;
-				count = 1;
+				if(count<10){
+					sprintf(cont,"%d",count);
+					strcpy(&newString[j],cont);
+					j++;
+					count = 1;
+				}else{
+					var1 = count%10;
+					var2 = var1%10;
+					sprintf(var3,"%d",var1);
+					strcpy(&newString[j],var3);
+					j++;
+					sprintf(var3,"%d",var2);
+					strcpy(&newString[j],var3);
+					j++;
+					count = 1;
+				}
+
 			}
 		}else{
 			count++;
@@ -29,4 +42,5 @@ int main(){
 	}
 	printf("frase: %s\n",newString);
 }
- 
+
+
